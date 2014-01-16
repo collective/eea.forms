669ce01fb86c8da202dc3dcb93d7435793352182
ichim-david <ichim.david@gmail.com>
2014-01-16 12:56:39 +0200

    Refs #17611; added themes fields to the categorization schema

--- eea/forms/content/categorization.py
+++ eea/forms/content/categorization.py
@@ -1,14 +1,23 @@
 """ EEA Base Schema
 """
+from Products.Archetypes.Field import LinesField
 
 from Products.Archetypes.Schema import Schema
 
 from datetime import datetime
+from Products.Archetypes.Widget import InAndOutWidget
 from eea.forms.fields.ManagementPlanField import ManagementPlanField
 from eea.forms.widgets.ManagementPlanWidget import ManagementPlanWidget
 
+try:
+    from eea.themecentre.content.ThemeTaggable import ThemesField
+except ImportError:
+    class ThemesField(LinesField):
+        """ Archetypes default LinesField
+        """
+        pass
 
-schema = Schema((
+categorization_schema = Schema((
 
     ManagementPlanField(
         name='eeaManagementPlan',
@@ -17,9 +26,9 @@ schema = Schema((
         required_for_published=True,
         required=False,
         default=(datetime.now().year, ''),
-        validators = ('management_plan_code_validator',),
+        validators=('management_plan_code_validator',),
         vocabulary_factory="Temporal coverage",
-        widget = ManagementPlanWidget(
+        widget=ManagementPlanWidget(
             format="select",
             label="EEA Management Plan",
             description=("EEA Management plan code. Internal EEA project "
@@ -29,7 +38,28 @@ schema = Schema((
             label_msgid='dataservice_label_eea_mp',
             description_msgid='dataservice_help_eea_mp',
             i18n_domain='eea.dataservice',
-            )
+        )
+    ),
+
+    ThemesField(
+        name='themes',
+        schemata='categorization',
+        validators=('maxValues',),
+        widget=InAndOutWidget(
+            maxValues=3,
+            label="Themes",
+            description="Choose max 3 themes",
+            label_msgid='EEAContentTypes_label_themes',
+            description_msgid='EEAContentTypes_help_themes',
+            i18n_domain='EEAContentTypes',
+        ),
+        languageIndependent=True,
+        vocabulary='_getMergedThemes',
+        index="KeywordIndex:brains",
+        enforceVocabulary=1,
+        default=[],
+        accessor='getThemes',
+        mutator='setThemes',
     ),
 ))
 
