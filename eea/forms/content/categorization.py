71ca172bd98f28a4d2c263335863c95b5b184bdb
ichim-david <ichim.david@gmail.com>
2014-01-17 15:38:03 +0200

    Refs #17611; added temporalCoverage field to categorization.py and removed double call to temporalFormat within temporal_macros.pt

--- eea/forms/content/categorization.py
+++ eea/forms/content/categorization.py
@@ -4,7 +4,7 @@ from Products.Archetypes.Field import LinesField
 
 from Products.Archetypes.Schema import Schema
 
-from Products.Archetypes.Widget import InAndOutWidget
+from Products.Archetypes.Widget import InAndOutWidget, MultiSelectionWidget
 
 try:
     from eea.themecentre.content.ThemeTaggable import ThemesField
@@ -35,5 +35,25 @@ categorization_schema = Schema((
         accessor='getThemes',
         mutator='setThemes',
     ),
+
+    LinesField(name='temporalCoverage',
+               languageIndependent=True,
+               schemata='categorization',
+               required=False,
+               multiValued=1,
+               vocabulary_factory=u"Temporal coverage",
+               widget=MultiSelectionWidget(
+                   macro="temporal_widget",
+                   helper_js=("temporal_widget.js",),
+                   size=15,
+                   label="Temporal coverage",
+                   description=("The temporal scope of the content of the data "
+                                "resource. Temporal coverage will typically "
+                                "include a set of years or time ranges."),
+                   label_msgid='dataservice_label_coverage',
+                   description_msgid='dataservice_help_coverage',
+                   i18n_domain='eea',
+               )
+    )
 ))
 
